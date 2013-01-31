97c215c8d332da6f6e3ed2fca322c3d626fd296a
Ryan Beck-Buysse <rbuysse@gmail.com>
2013-01-30 19:18:13 -0600

    checkifused() function was returning backwards. Fixed the dupe url code. Added redirerect for blank forms entries.

--- url.py
+++ url.py
@@ -27,7 +27,7 @@ def genkey(keylength):
         #TO-DO
         #if no key after 5 times, increase the keyspace by one.
         chars = "".join( [random.choice(string.letters) for i in xrange(keylength)] )
-        test = checkifused(key,chars)
+        test = checkifused("key",chars)
     return chars
 
 #tests to see if data is in the DB already. returns 0 if no, 1 if yes.
@@ -35,9 +35,9 @@ def checkifused(column, data):
     q = g.db.execute('SELECT dest FROM urls WHERE '+ column +' ="' + data + '"')
     checkval=q.fetchone()
     if checkval is None:
-        return "0"
-    else:
         return "1"
+    else:
+        return "0"
 
 @app.before_request
 def before_request():
@@ -56,22 +56,32 @@ def main():
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
-    if request.method == 'POST':
+    if request.method == 'POST' and request.form['dest_url']:
         #checking to see if the user entered a custom shortcode, test it and return error if it's already in use.
         if request.form['ccode']:
             newurlkey = request.form['ccode']
-            if checkifused(key,newurlkey) == 1:
-            return render_template("sorry.html") # need to create this page where it says the custom key is in use. display the code and URL.
+            if checkifused("key",newurlkey) == "1":
+                return render_template("sorry.html") # need to create this page where it says the custom key is in use. display the code and URL.
         else:
             newurlkey = genkey(5)
+
+        #Does the url include a protocol?
+        foorl = request.form['dest_url']
+        if foorl[0:8] != "https://" and foorl[0:7] != "http://":
+            foorl = "http://" + foorl
+
         #test if the entered URL is already there. We don't want dupes!
-        urltest = checkifused(dest,request.form['dest_url'])
-        if urltest == "1":
-            return render_template("sorry.html") # mention URL is already in use. give the short code.
+        urltest = checkifused("dest",foorl)
+        if urltest == "0":
+            q = g.db.execute('SELECT key FROM urls WHERE dest = ?',(foorl, ))
+            oldurlkey = q.fetchone()[0]
+            return render_template("added.html",linkurl=oldurlkey) # mention URL is already in use. give the short code.
         else:
-            g.db.execute('INSERT INTO urls (key, dest) values (?,?)',[newurlkey,request.form['dest_url']])
+            g.db.execute('INSERT INTO urls (key, dest) values (?,?)',[newurlkey,foorl])
             g.db.commit()
             return render_template('added.html', linkurl=newurlkey)
+    else:
+        return redirect(url_for('main'))
 
 @app.route('/<shortcode>')
 def redirecturl(shortcode): 
